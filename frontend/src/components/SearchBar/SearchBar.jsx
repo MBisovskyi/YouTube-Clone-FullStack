de39723317a59d519df248c56e33395dad4b5a19
unknown <mbisovskyi@gmail.com>
2022-08-16 14:49:26 -0400

    Created a SearchBar functionality

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const SearchBar = (props) => {
-    const [entry,setEntry] = useState('')
+  const [entry, setEntry] = useState("");
 
-    return (
-        <div>
-            <label>
-                <input type="text" value={entry} onChange={(event) => setEntry(event.target.value)}/>
-            </label>
-            <button onClick={setEntry}>Search</button>
-        </div>
-      );
-}
- 
+  async function handleSubmit() {
+    let response = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?q=${entry}&key=AIzaSyAry76oSZiXa8xlzDNvABmRxNFReBReodk
+      &part=snippet`
+    );
+    props.searchedVideos(response.data.items);
+    setEntry("");
+  }
+
+  return (
+    <div>
+      <label>
+        <input
+          type="text"
+          value={entry}
+          onChange={(event) => setEntry(event.target.value)}
+        />
+      </label>
+      <button onClick={handleSubmit}>Search</button>
+    </div>
+  );
+};
+
 export default SearchBar;
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -4,39 +4,47 @@ import useAuth from "../../hooks/useAuth";
 import axios from "axios";
 import SearchBar from "../../components/SearchBar/SearchBar";
 
-const HomePage = (props) => {
+const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
   const [videos, setVideos] = useState([]);
-  // const [entry,setEntry] = useState('')
 
   useEffect(() => {
-    async function getVideos () {
-      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=manunited&key=AIzaSyBZj6DOul-OAlEnTEeaL-ivV1zt5o2Ta90&part=snippet`);
-      setVideos(response.data.items)
-    }; getVideos()}, 
-  [token]);
-return (
-  <div>
-    <SearchBar parentEntries={props.entry}/>
-    <p>Welcome {user.username}!</p> 
-    <div>
-      {videos.map(function(vid){
-        return (
-          <div>
-            <div key={vid.id.videoId}>{vid.snippet.title}</div>
-            <iframe id="ytplayer" type="text/html" width="640" height="360"
-            src={`https://www.youtube.com/embed/${vid.id.videoId}`}
+    async function getVideos() {
+      let response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/search?q=manunited&key=AIzaSyAry76oSZiXa8xlzDNvABmRxNFReBReodk
+        &part=snippet`
+      );
+      setVideos(response.data.items);
+    }
+    getVideos();
+  }, [token]);
 
-            frameborder="0"></iframe>
-          </div>
-        );
-      })}
+  return (
+    <div>
+      <SearchBar searchedVideos={setVideos} />
+      <p>Welcome {user.username}!</p>
+      <div>
+        {videos.map(function (vid) {
+          return (
+            <div key={vid.id.videoId}>
+              <div>{vid.snippet.title}</div>
+              <iframe
+                id="ytplayer"
+                type="text/html"
+                width="640"
+                height="360"
+                src={`https://www.youtube.com/embed/${vid.id.videoId}`}
+                frameBorder="0"
+              ></iframe>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  </div>
-)
-}
+  );
+};
 
 export default HomePage;
