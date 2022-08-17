60d0a392de832c03ee8e81d47765f5520837731f
Ryan Etedali <ryanetedali@gmail.com>
2022-08-17 16:23:38 -0500

    completed the GET functionallity for the comments

--- frontend/src/components/Comment/CommentList/CommentList.jsx
+++ frontend/src/components/Comment/CommentList/CommentList.jsx
@@ -1,6 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+
 const CommentList = (props) => {
+    const { videoId } = useParams()
+    const [comments, setComments] = useState([])
+
+    useEffect(()=>{
+        displayComment()
+    })
+
+    async function displayComment() {
+       let response = await axios.get(`http://127.0.0.1:8000/api/comments/all/${videoId}/`);
+       console.log(response)
+       setComments(response.data)
+    }
+
     return ( 
-        <div></div>
+        <div>
+            <div>
+                {comments.map(function(comment, index){
+                    return (
+                        <div key={index}>
+                            <p>Name: {comment.user.username}</p>
+                            <div>
+                                Comment:<br/><p>{comment.text}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
      );
 }
  
